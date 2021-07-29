ee351eb30dfe5ef4ece271c8e97f1636e998c99f
poisongas <poisongass@yahoo.com>
2021-07-29 14:27:35 +0700

    hashbrowser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
